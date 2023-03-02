8d191b774613f8bf2b8b0990dd02cbe26be923eb
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-03-02 04:56:16 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.request
+import socket
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         # Configura a resposta HTTP
+        PORTs = 8080
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
 
         # Configura a resposta como uma string contendo código 
         link = self.path[1:]
@@ -18,6 +22,9 @@ class MyServer(BaseHTTPRequestHandler):
               l1=url[1]
               with urllib.request.urlopen(l1) as response:
                    html = response.read().decode()
+                   if url[1].find(".htm")>1:
+                        html=html.replace("http://","rrr1234567890://"+ip_address+":"+str(PORTs)+"/&rrr1234567890s://")
+                        html=html.replace("rrr1234567890","http")
 
         
         # Converte a string para bytes antes de enviá-la como resposta
@@ -25,11 +32,10 @@ class MyServer(BaseHTTPRequestHandler):
         return
 
 # Define a porta em que o servidor deve escutar
-PORT = 8000
+PORT = 8080
 print ("\033c\033[44;30m\n")
 # Cria um servidor que escuta na porta especificada e usa o manipulador de solicitações HTTP personalizado
 with HTTPServer(("", PORT), MyServer) as httpd:
     print("Servidor rodando na porta:", PORT)
     # Aguarda indefinidamente por solicitações HTTP
     httpd.serve_forever()
-

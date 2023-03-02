180e3e2866d0b73ff4ab80576d463e57806d14fe
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-03-02 14:50:21 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import socket
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         # Configura a resposta HTTP
+        
         PORTs = 8080
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -18,19 +19,36 @@ class MyServer(BaseHTTPRequestHandler):
         l1=""
         html=""
         url = link.split("&")
-        if len(url)>1 :
-              l1=url[1]
-              with urllib.request.urlopen(l1) as response:
-                   if l1.find(".png")<1 and l1.find(".jpg")<1 and l1.find(".bmp")<1:
-                        html = response.read().decode()
-                        html=html.replace("https://","rrr1234567890://"+ip_address+":"+str(PORTs)+"/&rrr1234567890s://")
-                        html=html.replace("./","rrr1234567890://"+ip_address+":"+str(PORTs)+"/&rrr1234567890s://")
-                        html=html.replace("rrr1234567890","http")
-                   else:
-                        html = response.read().decode()
+        try:
+            if len(url)>1 :
+                  l1=url[1]
+                  with urllib.request.urlopen(l1) as response:
+                       if l1.find(".png")<1 and l1.find(".jpg")<1 and l1.find(".bmp")<1:
+                           self.send_response(200)
+                           self.send_header('Content-type', 'text/html')
+                           self.end_headers()
+                           hostname = socket.gethostname()
+                           ip_address = socket.gethostbyname(hostname)
+                           html = response.read().decode()
+                           html=html.replace("https://","rrr1234567890://"+ip_address+":"+str(PORTs)+"/&rrr1234567890s://")
+                           html=html.replace("./","rrr1234567890://"+ip_address+":"+str(PORTs)+"/&rrr1234567890s://")
+                           html=html.replace("rrr1234567890","http")
+                           self.wfile.write(bytes(html, "utf8"))
+                       else:
+                           self.send_response(200)
+                           self.send_header('Content-Type', 'image/png')
+                           self.end_headers()
+                           hostname = socket.gethostname()
+                           ip_address = socket.gethostbyname(hostname)
+                           html = response.read().decode()
         
-        # Converte a string para bytes antes de enviá-la como resposta
-        self.wfile.write(bytes(html, "utf8"))
+                           # Converte a string para bytes antes de enviá-la como resposta
+                           self.wfile.write(bytes(html, "utf8"))
+        except:
+            try:
+                self.wfile.write(bytes(html, "utf8"))
+            except:
+               print ("error")
         return
 
 # Define a porta em que o servidor deve escutar
